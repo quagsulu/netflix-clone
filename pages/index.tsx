ce012e9fda876786a25f929b042e26f31b8b346a
quagsulu <quangnvph24106@fpt.edu.vn>
2023-11-19 13:29:38 +0700

    set up laij moi truong

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+
+  return (
+    <>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat repellendus molestiae dicta officiis illo facilis dolor itaque velit! Explicabo dolorem neque pariatur ab. Cumque, vero minima magnam ipsa eos labore.</h1>
+    </>
+  )
+}
+
+export default Home;
